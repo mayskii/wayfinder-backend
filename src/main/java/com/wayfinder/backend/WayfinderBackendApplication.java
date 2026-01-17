048e2b845d05a30564748e819a476030675ca481
Maya Zakharova <mayzakharova@gmail.com>
2026-01-17 17:52:11 -0600

    Initial Spring Boot project

---
+++ src/main/java/com/wayfinder/backend/WayfinderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wayfinder.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayfinderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WayfinderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wayfinder/backend/WayfinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wayfinder.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayfinderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
