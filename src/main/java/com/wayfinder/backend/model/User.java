06457578831f9163807c6fe2dd8c0b8105ab44bb
Maya Zakharova <mayzakharova@gmail.com>
2026-01-17 18:38:37 -0600

    create class User

---
+++ src/main/java/com/wayfinder/backend/model/User.java
@@ -0,0 +1,10 @@
+package com.wayfinder.backend.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+    public Integer id;
+    public String name;
+    public String email;
+    public LocalDateTime createdAt;
+}
